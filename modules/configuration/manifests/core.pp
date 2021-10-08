1d9dc342ed0c6bb34e02c92b5d5b9adfc94a4a7d
Hellia <kevin@kelaun.be>
2021-10-08 18:35:05 +0200

    Adding bash config

--- modules/configuration/manifests/core.pp
+++ modules/configuration/manifests/core.pp
@@ -15,4 +15,18 @@ class configuration::core {
     mode => '0644',
     source => 'https://raw.githubusercontent.com/hellia-be/dotfiles/master/pacman/pacman.conf',
   }
+  file { '/home/hellia/.bashrc':
+    ensure => present,
+    owner  => 'hellia',
+    group  => 'hellia',
+    mode   => '0644',
+    source => 'https://raw.githubusercontent.com/hellia-be/dotfiles/main/bash/bashrc',
+  }
+  file { '/home/hellia/.inputrc':
+    ensure => present,
+    owner  => 'hellia',
+    group  => 'hellia',
+    mode   => '0644',
+    source => 'https://raw.githubusercontent.com/hellia-be/dotfiles/main/bash/inputrc',
+  }
 }
