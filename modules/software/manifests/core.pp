7eea6c8b8b2df0f2dbc1ee34254b841fe8cb461c
Hellia <kevin@kelaun.be>
2021-10-08 18:08:57 +0200

    Adding gnome extensions

--- modules/software/manifests/core.pp
+++ modules/software/manifests/core.pp
@@ -57,4 +57,36 @@ class software::core {
   package { 'timeshift':
     ensure => installed,
   }
+  package { 'gnome-tweaks':
+    ensure => installed,
+  }
+  exec { 'gnome-shell-extension-arch-update':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save gnome-shell-extension-arch-update',
+    unless  => '/usr/bin/pacman -Qi gnome-shell-extension-arch-update',
+  }
+  exec { 'gnome-shell-extension-just-perfection-desktop-git':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save gnome-shell-extension-just-perfection-desktop-git',
+    unless  => '/usr/bin/pacman -Qi gnome-shell-extension-just-perfection-desktop-git',
+  }
+  exec { 'gnome-shell-extension-no-overview':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save gnome-shell-extension-no-overview',
+    unless  => '/usr/bin/pacman -Qi gnome-shell-extension-no-overview',
+  }
+  exec { 'gnome-shell-extension-nordvpn-connect-git':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save gnome-shell-extension-nordvpn-connect-git',
+    unless  => '/usr/bin/pacman -Qi gnome-shell-extension-nordvpn-connect-git',
+    require => Exec['nordvpn-bin'],
+  }
+  exec { 'nordvpn-bin':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save nordvpn-bin',
+    unless  => '/usr/bin/pacman -Qi nordvpn-bin',
+  }
+  exec { 'gnome-shell-extension-sound-output-device-chooser':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save gnome-shell-extension-sound-output-device-chooser',
+    unless  => '/usr/bin/pacman -Qi gnome-shell-extension-sound-output-device-chooser',
+  }
+  exec { 'gnome-shell-extension-tray-icons-reloaded-git':
+    command => '/usr/bin/yay -S --sudoloop --noconfirm --save gnome-shell-extension-tray-icons-reloaded-git',
+    unless  => '/usr/bin/pacman -Qi gnome-shell-extension-tray-icons-reloaded-git',
+  }
 }
