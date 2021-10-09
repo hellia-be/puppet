3b9fb986ecb6a7ad8d33ea2bd20aa7439dbfdcd7
hellia-be <91613249+hellia-be@users.noreply.github.com>
2021-10-09 12:01:02 +0200

    Update office.pp

--- modules/software/manifests/office.pp
+++ modules/software/manifests/office.pp
@@ -2,9 +2,6 @@ class software::office {
   package { 'libreoffice-fresh':
     ensure => installed,
   }
-  package { 'transmission-gtk':
-    ensure => installed,
-  }
   package { 'fractal':
     ensure => installed,
   }
