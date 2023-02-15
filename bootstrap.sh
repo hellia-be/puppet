5111aec65b4869e63044eca0e53f75583a703cd1
hellia <hellia@kelaun.be>
2023-02-15 11:57:39 +0100

    fix modulepath

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@
 sudo pacman -Syyuu
 
 sudo pacman -S puppet git
-su -c "cd /etc && rm -rf puppet && git clone https://github.com/hellia-be/puppet.git && cd puppet && puppet module install puppet-archive --version 6.1.1"
+su -c "cd /etc && rm -rf puppet && git clone https://github.com/hellia-be/puppet.git && cd puppet && puppet module install puppet-archive --version 6.1.1 --modulepath=/etc/puppet/modules"
